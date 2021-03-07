ecb8cf59cfc7658833e5ad56fca7fb55370a2b05
chris8736 <christophertam88@gmail.com>
2021-03-07 12:13:59 -0500

    add follows model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Follow.delete_all
+
+30.times do
+    r = Follow.create(user_id: 1, follower_id: 1)
+    #r = Follow.create(user_id: User.all.sample.id, follower_id: User.all.sample.id)
+end
