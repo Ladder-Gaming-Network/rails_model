432b367df6cf65ea472fc1a96603a7408bd739f0
chris8736 <christophertam88@gmail.com>
2021-03-27 20:03:26 -0400

    more css changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,10 @@ class StaticPagesController < ApplicationController
   end
 
   def follow
-    puts("Button pressed!")
-    Follow.create(user_id: 1, follower_id: 2)
+    Follow.create(user_id: params[:user_id], follower_id: 1)
+  end
+
+  def unfollow
+    Follow.where(user_id: params[:user_id], follower_id: 1).destroy_all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/profile'
   get '/profile/follow', to: 'static_pages#follow'
+  get '/profile/unfollow', to: 'static_pages#unfollow'
   resources :users
   resources :posts
   resources :follows
