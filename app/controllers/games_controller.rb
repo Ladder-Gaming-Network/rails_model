ce5f1ce9efe8e17f5eba4caf8a1b13860f82772c
Jacob Smith <jsmith2021@brandeis.edu>
2021-05-04 02:02:45 -0400

    minor fix to show games, code was hardcoded

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,7 @@ class GamesController < InheritedResources::Base
   # GET /games/1 or /games/1.json
   def show
     @game = Game.find(params[:id])
-    game_api=SteamWebApi::Game.new(@game.code.to_s)
-    game_api=SteamWebApi::Game.new(1361320)
+    game_api=SteamWebApi::Game.new(@game.code)
     @achievments=game_api.achievement_percentages.achievements
     @news=game_api.news.news_items
   end
